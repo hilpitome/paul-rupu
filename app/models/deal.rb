77ed34801a3b470cee4e8df137ddbaf49af937e8
hilpitome <hilpitome@gmail.com>
2015-09-18 09:10:40 +0300

    connected deal, cart and line-items

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -3,4 +3,18 @@ class Deal < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   self.inheritance_column = :foo
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  private
+
+# ensure that there are no line items referencing this deal
+  def ensure_not_referenced_by_any_line_item
+       if line_items.empty?
+        return true
+       else
+        errors.add(:base, 'Line Items present')
+       return false
+       end
+  end
+
 end
