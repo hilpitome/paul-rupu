9705dc0588be76c930dbca6f1fca89d4e310c187
hilpitome <hilpitome@gmail.com>
2015-10-31 22:56:35 +0300

    modified orders form layout

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,14 +4,14 @@ class Cart < ActiveRecord::Base
   def add_deal(deal_id)
    current_item = line_items.find_by(deal_id: deal_id)
    if current_item
-   current_item.quantity += 1
+    current_item.quantity += 1
    else
-   current_item = line_items.build(deal_id: deal_id)
+    current_item = line_items.build(deal_id: deal_id)
    end
-   current_item
+    current_item
   end
 
   def total_price
-  line_items.to_a.sum { |line_item| line_item.total_price }
+   line_items.to_a.sum { |line_item| line_item.total_price }
   end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,9 +3,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :deal
   belongs_to :cart
 
- # def to_s
- #   title
- # # end
+
  def total_price
   deal.price * quantity
  end
