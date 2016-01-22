e2b34059b88382a2fcb5a7efb5288343f46e50cc
hilpitome <hilpitome@gmail.com>
2016-01-22 23:40:13 +0300

    tinkered with action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -7,17 +7,12 @@ Rails.application.initialize!
 HilaOnline::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:
-  "smtp.gmail.com",
-  port:
-  587,
-  domain:
-  "domain.of.sender.net",
-  authentication: "plain",
-  user_name:
-  "dave",
-  password:
-  "secret",
-  enable_starttls_auto: true
-  }
+ :address              => "smtp.gmail.com",
+ :port                 => 25,
+ :user_name            => ENV['gmail_username'],
+ :password             => ENV['gmail_password'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
+
 end
